a690d49fbd64d13e4376d7f50e5209a1e8403bf6
schroederb <benjamin-ries@outlook.com>
2021-12-21 00:30:02 +0100

    update

--- reffix/entry_rules.py
+++ reffix/entry_rules.py
@@ -1,5 +1,18 @@
+from collections import OrderedDict
+
+import nltk
+nltk.download('wordnet')
+
+import iso4
+from titlecase import titlecase
+
+
 def titelcasing_fields(record):
-    record.update({"title": titlecase(record['title'].replace("{", "").replace("}", ""))})
+    record.update({"title": "{\\em "+titlecase(record['title']+" \\em}")})
+
+    if("journal" in record):
+        record.update({"journal": titlecase(record['journal'])})
+    
     return record
 
 
@@ -28,6 +41,120 @@ def capitalize_keys(record):
     record.update({'ID': record['ID'].title()})
     return record
 
+def fixLatexChars(record):
+    comment_list = ["&", "_"]
+    
+    replacmentDict = OrderedDict({
+        "\\textasciigrave": "^",
+        "\\textbackslash": "\\",
+        "\\textquotesingle": "'",
+        "\\textdollar ":"$",  
+        "\\textendash":"-",
+        "{\\L}ambda":"\\lambda",
+        "$\\{B}eta ": "$\\beta",
+        "{D}na": "{DNA}",
+        "{D}NA": "{DNA}",
+        "{Q}m": "{QM}",
+        "{Q}M": "{QM}",
+        "{M}m": "{MM}",
+        "{M}D": "{MD}",
+        "{M}d": "{MD}",
+        "{I}i": "{II}",
+        "{I}I": "{II}",
+        "{N}mr": "{NMR}",
+        "{N}MR": "{NMR}",
+         "N{M}r": "{NMR}",
+        " {E}m ": "{EM}",
+        " {E}M ": "{EM}",
+        "{A}MB{E}R": "{AMBER}",
+        "{G}AF{F}": "{GAFF}",
+        "{A}TB": "{ATB}",
+        "C{H}AR{M}M": "{CHARMM}",
+        "{O}PL{S}" : "{OPLS}",
+        "{S}HA{K}E": "{SHAKE}",
+        "{R}AT{T}LE": "{RATTLE}",
+        "P{H}": "{PH}",
+        "{O}pen{M}M": "{OpenMM}",
+        "{S}T2": "{ST}2",
+        "{R}e-{E}ds": "{RE}-{EDS}",
+        "{Q}lig{F}EP": "{QligFEP}",
+        "{D}3R": "{D3R}",
+        "{N}-Methyltransferase" : "{N}-{M}ethyltransferase",
+        "{N}AM{D}":"{NAMD}",
+        "{L}eu{s}": "{LEUS}",
+        "{Q}res{F}EP":"{QresFEP}",
+        "{G}B3":"{GB}3",
+        "{M}DF{P}": "{MDFP}",
+        "H{T}RP{V}1": "{HTRPV1}",
+        "{A}T1":"{AT1}",
+        "F{E}Setup": "{FESetup}",
+        "{P}yAuto{F}EP": "{P}y{A}uto{F}EP",
+        "F{E}W": "{FEW}",
+        "{P}mx": "{Pmx}",
+        "{E}DS": "{EDS}",
+        "{A}ZD": "{AZD}",
+        "{P}yE{M}MA":"{P}y{EMMA}",
+        "M{D}Traj":"{MDT}raj",
+        "Eds": "{EDS}",
+        "{O}ME{G}A": "{OMEGA}",
+        "Rdkit": "{R}dkit",
+        "{J}SO{N}": "{JSON}",
+        "Methylations":"{M}ethylations",
+        "{M}s-$\Lambda$-{L}eu{s}" : "{MS}-$\lambda$-{LEUS}",
+        "S{A}MP{L}4": "{SAMPL}4",
+        "G{R}OM{O}S": "{GROMOS}",
+        "{G}RO{M}OS": "{GROMOS}",
+        "B{R}o5":"{bRO}5",
+        "{C}HK1":"{CHK1}",
+        "{S}pc":"{SPC}",
+        "L{l}vm": "{LLVM}",
+        "{J}IT": "{JIT}",
+        "Wire": "WIRE",
+        "{G}mx": "{GMX}",
+        "L{A}MM{P}S": "{LAMMPS}",
+        "A{P}I":"{API}",
+        "H{D}F": "{HDF}",
+        "{R}inikerlab/{P}yGromos{T}ools: {P}yGromos{T}ools": "rinikerlab/pygromostools: {P}y{G}romos{T}ools",
+        "arXiv": "ar{X}iv",
+        "{C}oM{F}A": "{CoMFA}",
+        "{C}oM{S}IA": "{CoMSIA}",
+        "3d-{Q}sar": "3{D}-{QSAR}",
+        "ä" : "\\\"a", 
+        "Ä" : "\\\"A",
+        "ö" : "\\\"o",
+        "Ö" : "\\\"O",
+        "ü" : "\\\"u",
+        "Ü" : "\\\"U",
+        "è" : "\\\'e",
+        "à" : "\\\'a",
+        "é" : "\\\`e",
+        "á" : "\\`a",
+        'ç' : '\\c{c}',
+        'ô' : '\\^{o}',
+        "ŏ" : '\\u{o}',
+        "É" : "{\\'E}",
+        "ß" : "{\ss}", 
+        "\\\\":"\\",
+
+    })
+    
+    for field_key in record:
+        field = record[field_key]
+        for replace in comment_list:
+            if(replace in field):
+                for i,c in enumerate(field):
+                    if(replace == c and field[i-1] !="\\"):
+                        field = field[:i]+"\\"+field[i:]
+        record.update({field_key:field})
+            
+    for field_key in record:
+        field = record[field_key]
+        if(any([key in field for key in replacmentDict])):
+            for replace in replacmentDict:
+                field = field.replace(replace, replacmentDict[replace])
+            record.update({field_key:field})
+
+    return record
 
 # --------------------------------------------------------------------------------
 # Checking Fields
@@ -80,7 +207,7 @@ def check_inproceedings_fields(record, fields=("title", "author", "year"),
     return record
 
 
-def check_inbook_fields(record, fields=("title", "author", "year"), optional=("publisher", "pages", "booktitle")):
+def check_inbook_fields(record, fields=("title", "author", "year", "chapter", "pages"), optional=("publisher", "pages", "booktitle")):
     if (record['ENTRYTYPE'] == "inbook"):
         try:
             newRecord = {field: record[field] for field in fields}
